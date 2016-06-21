f8e2bffce004f8c5ef06d00bde0a7a863b214407
Flavykenfack <flavienn.kenfack@he-arc.ch>
2016-06-22 01:00:31 +0200

    Update Header Body
    Update the view of CV, try to add color in header some border in grid.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,17 @@
 console.log('\'Allo \'Allo!');
 
-$(document).ready(function()
-	$('a').smoothScroll();
-	);
+// $(document).ready(function()
+// 	$('a').smoothScroll();
+// 	);
+
+$('a[href^="#"]').click(function(){
+	var the_id = $(this).attr("href");
+
+	$('html, body').animate({
+		scrollTop:$(the_id).offset().top
+	}, 'slow');
+	return false;
+});
+
+
+$.support.transition = true;
