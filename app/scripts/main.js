ef948ac6d3b1a839c0f361bd6b84f45c9b619028
Flavykenfack <flavienn.kenfack@he-arc.ch>
2016-06-23 23:19:00 +0200

    Correct app/index.html, add canvas, progress bar
    By mistake i deleted all data that are in app/index.html and i corrected
    it by copy data in the last commit "add carrousel"
    I succeed to create a doughnut canvas with chart.js and added a progess
    bar
    I begin the function to transform progress bar to canvas

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,17 +1,39 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function(){
 
-// $(document).ready(function()
-// 	$('a').smoothScroll();
-// 	);
+var data = {
+    labels: [
+        "Red",
+        "Blue",
+        "Yellow"
+    ],
+    datasets: [
+        {
+            data: [300, 50, 100],
+            backgroundColor: [
+                "#FF6384",
+                "#36A2EB",
+                "#FFCE56"
+            ],
+            hoverBackgroundColor: [
+                "#FF6384",
+                "#36A2EB",
+                "#FFCE56"
+            ]
+        }]
+};
 
-$('a[href^="#"]').click(function(){
-	var the_id = $(this).attr('href');
 
-	$('html, body').animate({
-		scrollTop:$(the_id).offset().top
-	}, 'slow');
-	return false;
+//var ctx = document.getElementById("myCanvas").getContext("2d");
+var ctx = $("#myCanvas").get(0).getContext("2d");
+var myCanvas = new Chart(ctx, {
+	type: 'doughnut',
+	data: data,
 });
 
+$function myCanvasTransform (argument) {
+	// body...
+}
 
-$.support.transition = true;
+
+});
+
