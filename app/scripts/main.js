a1b5f7daff9df5297066dcb72944a6063a53c730
Flavykenfack <flavienn.kenfack@he-arc.ch>
2016-06-26 12:19:11 +0200

    Add progress bar
    Add 6 progress bar. customize code

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,9 +2,7 @@ $(document).ready(function(){
 
 var data = {
     labels: [
-        "Red",
-        "Blue",
-        "Yellow"
+        "Red", "Blue", "Yellow"
     ],
     datasets: [
         {
@@ -22,12 +20,13 @@ var data = {
         }]
 };
 
-
-//var ctx = document.getElementById("myCanvas").getContext("2d");
 var ctx = $("#myCanvas").get(0).getContext("2d");
 var myCanvas = new Chart(ctx, {
 	type: 'doughnut',
 	data: data,
+    options: {
+        responsive: true
+    }
 });
 
 $function myCanvasTransform (argument) {
