a1194757b5069f3235e57774e234594fdd5a7f44
Flavykenfack <flavienn.kenfack@he-arc.ch>
2016-06-26 14:02:45 +0200

    Change Navbar
    changed NavBar Theme

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,24 +2,19 @@ $(document).ready(function(){
 
 var data = {
     labels: [
-        "Red", "Blue", "Yellow"
+        "Red",
+        "Blue",
+        "Yellow"
     ],
     datasets: [
         {
-            data: [300, 50, 100],
-            backgroundColor: [
-                "#FF6384",
-                "#36A2EB",
-                "#FFCE56"
-            ],
-            hoverBackgroundColor: [
-                "#FF6384",
-                "#36A2EB",
-                "#FFCE56"
-            ]
+            data: [30, 50, 10],
+           
         }]
 };
 
+
+//var ctx = document.getElementById("myCanvas").getContext("2d");
 var ctx = $("#myCanvas").get(0).getContext("2d");
 var myCanvas = new Chart(ctx, {
 	type: 'doughnut',
